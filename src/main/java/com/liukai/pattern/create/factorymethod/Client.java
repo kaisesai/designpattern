ce1dfcbf4cedfebf16c116f2e026c719371c5dc5
liukai <liukai@jiedaibao.com>
2016-07-17 16:00:07 +0800

    初始化项目

--- src/main/java/com/liukai/pattern/create/factorymethod/Client.java
+++ src/main/java/com/liukai/pattern/create/factorymethod/Client.java
@@ -7,13 +7,14 @@ import java.sql.SQLException;
 
 public class Client {
 
-    private static QueryRunner queryRunner;
+	private static QueryRunner queryRunner;
 
-    public static void main(String[] args) throws SQLException {
-        //创建一个工厂类
-        queryRunner = new OracleQueryRunner();
-        ResultSet resultSet = queryRunner.run();
-    }
+	public static void main(String[] args) throws SQLException {
+		// 创建一个工厂类
+		queryRunner = new OracleQueryRunner();
+		ResultSet resultSet = queryRunner.run();
+		System.out.println(resultSet);
+	}
 
 }
 
@@ -26,79 +27,76 @@ public class Client {
  */
 abstract class QueryRunner {
 
-    /**
-     * 执行查询
-     *
-     * @return 抽象结果集
-     */
-    public ResultSet run() throws SQLException {
-        Connection connection = createConnection();
-        String sql = createSql();
-        return runSql(connection, sql);
-    }
+	/**
+	 * 执行查询
+	 *
+	 * @return 抽象结果集
+	 */
+	public ResultSet run() throws SQLException {
+		Connection connection = createConnection();
+		String sql = createSql();
+		return runSql(connection, sql);
+	}
 
-    /**
-     * 创建连接
-     *
-     * @return 抽象连接器
-     */
-    protected abstract Connection createConnection();
+	/**
+	 * 创建连接
+	 *
+	 * @return 抽象连接器
+	 */
+	protected abstract Connection createConnection();
 
-    /**
-     * 创建SQL语句
-     *
-     * @return
-     */
-    protected abstract String createSql();
+	/**
+	 * 创建SQL语句
+	 *
+	 * @return
+	 */
+	protected abstract String createSql();
 
-    /**
-     * 运行SQL语句
-     *
-     * @param connection
-     * @param sql
-     * @return 抽象结果集
-     */
-    protected abstract ResultSet runSql(Connection connection, String sql) throws SQLException;
+	/**
+	 * 运行SQL语句
+	 *
+	 * @param connection
+	 * @param sql
+	 * @return 抽象结果集
+	 */
+	protected abstract ResultSet runSql(Connection connection, String sql) throws SQLException;
 
 }
 
 /**
- * 具体工厂类
- * Sybase查询器
+ * 具体工厂类 Sybase查询器
  */
 class SybaseQueryRunner extends QueryRunner {
 
-    protected Connection createConnection() {
-        return null;
-    }
+	protected Connection createConnection() {
+		return null;
+	}
 
-    protected String createSql() {
-        return "select * from customers";
-    }
+	protected String createSql() {
+		return "select * from customers";
+	}
 
-    protected ResultSet runSql(Connection connection, String sql) throws SQLException {
-        PreparedStatement ps = connection.prepareStatement(sql);
-        return ps.executeQuery();
-    }
+	protected ResultSet runSql(Connection connection, String sql) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(sql);
+		return ps.executeQuery();
+	}
 }
 
 /**
- * 具体工厂类
- * Oracle查询器
+ * 具体工厂类 Oracle查询器
  */
 class OracleQueryRunner extends QueryRunner {
 
-    protected Connection createConnection() {
-        return null;
-    }
+	protected Connection createConnection() {
+		return null;
+	}
 
-    protected String createSql() {
-        return "select * from customers";
-    }
+	protected String createSql() {
+		return "select * from customers";
+	}
 
-    protected ResultSet runSql(Connection connection, String sql) throws SQLException {
-        PreparedStatement ps = connection.prepareStatement(sql);
-        return ps.executeQuery();
-    }
+	protected ResultSet runSql(Connection connection, String sql) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(sql);
+		return ps.executeQuery();
+	}
 }
-
